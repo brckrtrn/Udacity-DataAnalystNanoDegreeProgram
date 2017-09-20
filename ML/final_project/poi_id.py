9b07f6239537fa9e4fa28c6ef7fbc7641f9585c5
brckrtrn <brckrtrn@users.noreply.github.com>
2017-09-20 23:07:10 +0300

    Add files via upload

--- ML/final_project/poi_id.py
+++ ML/final_project/poi_id.py
@@ -16,7 +16,22 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score
 from numpy import mean
 from sklearn import preprocessing
 
+from sklearn.feature_selection import f_classif
 
+from sklearn import grid_search
+from sklearn import tree
+from sklearn.pipeline import Pipeline
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier
+
+from tester import test_classifier,dump_classifier_and_data
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
@@ -84,23 +99,23 @@ for i, feature in enumerate(features_list):
 print ''
 
 ### total missing values in each feature
-total_nan_features_count=0
+total_nan_features_count = 0
 for i, feature in enumerate(features_list):
-    total_nan_features_count=total_nan_features_count+nan_in_features[i]
+    total_nan_features_count = total_nan_features_count + nan_in_features[i]
 print "Number of NaN values:", total_nan_features_count
 print ''
 
 ### total non missing values in each feature
 total_non_nan_features_count=0
 for i, feature in enumerate(features_list):
-    total_non_nan_features_count=total_non_nan_features_count+non_nan_in_features[i]
+    total_non_nan_features_count = total_non_nan_features_count + non_nan_in_features[i]
 print "Number of not NaN values:", total_non_nan_features_count
 print ''
 
 ### total data point
 total_features_count=0
 for i, feature in enumerate(features_list):
-    total_features_count=total_features_count+nan_in_features[i]+non_nan_in_features[i]
+    total_features_count = total_features_count + nan_in_features[i] + non_nan_in_features[i]
 print "Number of total data points:", total_features_count
 print ''
 
@@ -116,7 +131,7 @@ def plotOutliers(data_set, feature_x, feature_y):
 #   matplotlib.pyplot.show()  #open this line to show plot
 print(plotOutliers(data_dict, 'salary', 'bonus'))
 
-print "looks like there's at least one outlier"
+print "looks like outlier could be found"
 
 ##persons with a salary greater than $600K
 print ''
@@ -157,12 +172,7 @@ for l in data_dict:
             i = data_dict[l]["bonus"]
             big_bonus = l
 print big_bonus
-# total is obviously an outlier. Remove it
-
-print ''
-print "'Total' is obviously an outlier so we will remove it from the dataset"
-data_dict.pop("TOTAL",0)
-print ''
+# total is obviously an outlier. IT will be removed
 
 #the second largest salary
 print "and the second largest salary goes to:"
@@ -189,6 +199,7 @@ print big_bonus
 
 #THE TRAVEL AGENCY IN THE PARK: this records does not represent a person .
 #LOCKHART EUGENE E: This record does not contain any information.
+
 print ''
 print "THE TRAVEL AGENCY IN THE PARK and Eugene Lockhart have no entered data:"
 print ''
@@ -197,35 +208,37 @@ print ''
 print data_dict["LOCKHART EUGENE E"]
 print ''
 
+print ''
+print "'Total' is obviously an outlier so we will remove it from the dataset"
+print ''
 print "We've decided to remove two entries more:"
 print "THE TRAVEL AGENCY IN THE PARK, and EUGENE LOCKHART"
-#remove THE TRAVEL AGENCY IN THE PARK and LOCKHART EUGENE
+
+#remove TOTAL, THE TRAVEL AGENCY IN THE PARK and LOCKHART EUGENE
+data_dict.pop("TOTAL",0)
 data_dict.pop("THE TRAVEL AGENCY IN THE PARK",0)
 data_dict.pop("LOCKHART EUGENE E",0)
-
 
 
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 
-
 my_dataset = data_dict
 
-#create new features
+#create new features - transformation of “from_this_person_to_poi” and “from_poi_to_this_person” values to ratios
 for keys, values in data_dict.iteritems():
     if values["to_messages"] != "NaN" and \
-                    values["from_messages"] != "NaN" and \
-                    values["from_poi_to_this_person"] != "NaN" and \
-                    values["from_this_person_to_poi"] != "NaN":
+       values["from_messages"] != "NaN" and \
+       values["from_poi_to_this_person"] != "NaN" and \
+       values["from_this_person_to_poi"] != "NaN":
 
-        values["poi_to_person_rate"] = float(values["from_poi_to_this_person"]) / \
-                                       values["to_messages"]
-        values["person_to_poi_rate"] = float(values["from_this_person_to_poi"]) / \
-                                       values["from_messages"]
+       values["poi_to_person_rate"] = float(values["from_poi_to_this_person"]) / values["to_messages"]
+       values["person_to_poi_rate"] = float(values["from_this_person_to_poi"]) / values["from_messages"]
     else:
-        values["poi_to_person_rate"] = 0
-        values["person_to_poi_rate"] = 0
-#
+       values["poi_to_person_rate"] = 0
+       values["person_to_poi_rate"] = 0
+
+#add new ones to features
 features_list.extend (['poi_to_person_rate', 'person_to_poi_rate'])
 
 print ""
@@ -233,17 +246,20 @@ print ""
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
-#Removes all but the k highest scoring features
-from sklearn.feature_selection import f_classif
+#Removes all but the k highest scoring features - after manually tries '7' is selected for k -
+
 k = 7
 selector = SelectKBest(f_classif, k=7)
 selector.fit_transform(features, labels)
 print("the score of each features:")
+
 scores = zip(features_list[1:],selector.scores_)
 sorted_scores = sorted(scores, key = lambda x: x[1], reverse=True)
 pprint.pprint(sorted_scores)
 optimized_features_list = poi_label + list(map(lambda x: x[0], sorted_scores))[0:k]
+
 print(optimized_features_list)
+
 #create new features list without new features
 without_new_features_list =['poi', 'exercised_stock_options', 'total_stock_value', 'bonus',
                             'salary', 'deferred_income', 'long_term_incentive']
@@ -267,7 +283,7 @@ new_features = scaler.fit_transform(new_features)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point without new features. Try a variety of classifiers.
-from sklearn.model_selection import train_test_split
+
 feature_train, feature_test, labels_train, labels_test = \
 	train_test_split( features, labels, test_size=0.3, random_state=42)
 
@@ -275,16 +291,22 @@ feature_train, feature_test, labels_train, labels_test = \
 new_feature_train, new_feature_test, new_labels_train, new_labels_test = \
 	train_test_split( new_features, new_labels, test_size=0.3, random_state=42)
 
-from sklearn import grid_search
-from sklearn import tree
-from sklearn.pipeline import Pipeline
-from sklearn.decomposition import PCA
-from sklearn.svm import SVC
-
 print ""
 
 # best paramaters find
-## tuning paramaters of decision trees
+## tuning parmaters of SVM
+
+SVM_clf = SVC()
+s_param = {'kernel': ['rbf', 'linear', 'poly'], 'C': [ 0.1,1,10, 100, 1000],\
+           'gamma': [1, 0.1, 0.01, 0.001, 0.0001], 'random_state': [42]}
+s_clf = grid_search.GridSearchCV(SVM_clf, s_param)
+s_clf = s_clf.fit(features, labels)
+s_clf = s_clf.best_estimator_
+
+print s_clf
+
+# best paramaters find
+## tuning paramaters of Decision Trees
 
 parameters = {'min_samples_leaf':[10,20,30,40,50], 'min_samples_split':[20,30,40,50]}
 
@@ -297,18 +319,6 @@ print clf
 
 print ""
 
-# best paramaters find
-## tuning parmaters of SVM
-
-SVM_clf_1 = SVC()
-s_param = {'kernel': ['rbf', 'linear', 'poly'], 'C': [ 0.1,1,10, 100, 1000],\
-           'gamma': [1, 0.1, 0.01, 0.001, 0.0001], 'random_state': [42]}
-s_clf = grid_search.GridSearchCV(SVM_clf_1, s_param)
-s_clf = s_clf.fit(features, labels)
-s_clf = s_clf.best_estimator_
-
-print s_clf
-
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
@@ -316,46 +326,22 @@ print s_clf
 ### stratified shuffle split cross validation. For more info:
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
-#Naive bayes
-
-from sklearn.naive_bayes import GaussianNB
-GNB_clf = GaussianNB()
-new_GNB_clf = GaussianNB()
-
-#SVM
-
-from sklearn.svm import SVC
-SVM_clf = SVC(kernel='poly', C=1,gamma=1,random_state=42)
-new_SVM_clf = SVC(kernel='poly', C=1,gamma=1,random_state=42)
-
-#Decision Tree
-
-from sklearn.tree import DecisionTreeClassifier
-DT_clf = DecisionTreeClassifier(min_samples_split = 20,min_samples_leaf=20)
-new_DT_clf = DecisionTreeClassifier(min_samples_split = 20,min_samples_leaf=20)
-
-#AdaBoost
-
-from sklearn.ensemble import AdaBoostClassifier
-AdaB_clf = AdaBoostClassifier(algorithm='SAMME')
-new_AdaB_clf = AdaBoostClassifier(algorithm='SAMME')
-
-
 #Evaluate the models
-def evaluate_clf(clf, features, labels, num_iters=1000, test_size=0.3):
-    print clf
-    accuracy = []
-    precision = []
-    recall = []
+def model_evaluate(clf, features, labels, num_iters=1000, test_size=0.3):
+    arr_accuracy = []
+    arr_precision = []
+    arr_recall = []
     first = True
+	
     for trial in range(num_iters):
-        features_train, features_test, labels_train, labels_test = \
-            train_test_split(features, labels, test_size=test_size)
+        features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=test_size)
         clf.fit(features_train, labels_train)
         predictions = clf.predict(features_test)
-        accuracy.append(accuracy_score(labels_test, predictions))
-        precision.append(precision_score(labels_test, predictions))
-        recall.append(recall_score(labels_test, predictions))
+		
+        arr_accuracy.append(accuracy_score(labels_test, predictions))
+        arr_precision.append(precision_score(labels_test, predictions))
+        arr_recall.append(recall_score(labels_test, predictions))
+		
         if trial % 10 == 0:
             if first:
                 sys.stdout.write('\nProcessing')
@@ -363,51 +349,73 @@ def evaluate_clf(clf, features, labels, num_iters=1000, test_size=0.3):
             sys.stdout.flush()
             first = False
 
-    print "done.\n"
-    print "precision: {}".format(mean(precision))
-    print "recall:    {}".format(mean(recall))
-    print "accuracy:  {}".format(mean(accuracy))
-    return mean(precision), mean(recall),mean(accuracy)
+	#print results		
+	print clf
+    print "precision: {}".format(mean(arr_precision))
+    print "recall:    {}".format(mean(arr_recall))
+    print "accuracy:  {}".format(mean(arr_accuracy))
+	
+    return mean(arr_precision), mean(arr_recall),mean(arr_accuracy)
 
-#Evaluate navie bayes model without new_features
-print ""
-print("Evaluate navie bayes model without new_features")
-evaluate_clf(GNB_clf, features, labels)
+#Naive bayes
 
-#Evaluate navie bayes model with new_features
-print ""
-print("Evaluate navie bayes model with new_features")
-evaluate_clf(new_GNB_clf, new_features, new_labels)
+GNB_clf_org = GaussianNB()
+GNB_clf_new = GaussianNB()
 
-#Evaluate svm model without new_features
-print ""
-print("Evaluate svm model without new_features")
-evaluate_clf(SVM_clf, features, labels)
+#SVM
 
-#Evaluate svm model with new_features
-print ""
-print("Evaluate svm model with new_features")
-evaluate_clf(new_SVM_clf, new_features, new_labels)
+SVM_clf_org = SVC(kernel='poly', C=1,gamma=1,random_state=42)
+SVM_clf_new = SVC(kernel='poly', C=1,gamma=1,random_state=42)
 
-#Evaluate Decision Tree model without new_features
-print ""
-print("Evaluate Decision Tree model without new_features")
-evaluate_clf(DT_clf, features, labels)
+#Decision Tree
 
-#Evaluate Decision Tree model with new_features
-print ""
-print("Evaluate Decision Tree model with new_features")
-evaluate_clf(new_DT_clf, new_features, new_labels)
+DT_clf_org = DecisionTreeClassifier(min_samples_split = 20,min_samples_leaf=20)
+DT_clf_new = DecisionTreeClassifier(min_samples_split = 20,min_samples_leaf=20)
 
-#Evaluate Adaboost model without new_features
-print ""
-print("Evaluate Adaboost model without new_features")
-evaluate_clf(AdaB_clf, features, labels)
+#AdaBoost
 
-#Evaluate Adaboost model with new_features
+AdaB_clf_org = AdaBoostClassifier(algorithm='SAMME')
+AdaB_clf_new = AdaBoostClassifier(algorithm='SAMME')
+
+#Navie Bayes model without new features
 print ""
-print("Evaluate Adaboost model with new_features")
-evaluate_clf(new_AdaB_clf, new_features, new_labels)
+print("Evaluate navie bayes model without new feature")
+model_evaluate(GNB_clf_org, features, labels)
+
+#Navie Bayes model with new feature
+print ""
+print("Evaluate navie bayes model with new feature")
+model_evaluate(GNB_clf_new, new_features, new_labels)
+
+#SVM model without new feature
+print ""
+print("Evaluate svm model without new feature")
+model_evaluate(SVM_clf_org, features, labels)
+
+#VM model with new feature
+print ""
+print("Evaluate svm model with new feature")
+model_evaluate(SVM_clf_new, new_features, new_labels)
+
+#Decision Tree model without new feature
+print ""
+print("Evaluate Decision Tree model without new feature")
+model_evaluate(DT_clf_org, features, labels)
+
+#Decision Tree model with new feature
+print ""
+print("Evaluate Decision Tree model with new feature")
+model_evaluate(DT_clf_new, new_features, new_labels)
+
+#Adaboost model without new feature
+print ""
+print("Evaluate Adaboost model without new feature")
+model_evaluate(AdaB_clf_org, features, labels)
+
+#Adaboost model with new feature
+print ""
+print("Evaluate Adaboost model with new feature")
+model_evaluate(AdaB_clf_new, new_features, new_labels)
 print ""
 
 # Example starting point. Try investigating other evaluation techniques!
@@ -416,7 +424,6 @@ print ""
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
 
-from tester import test_classifier,dump_classifier_and_data
 print ""
 test_classifier(GNB_clf, my_dataset, without_new_features_list)
 print ""
